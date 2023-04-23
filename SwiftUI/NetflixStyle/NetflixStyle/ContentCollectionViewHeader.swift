57b3f7f93df5b74496f1c871030d0039d5de7f48
al0214 <sisang951@hansei.sen.hs.kr>
2023-04-23 21:09:09 +0900

    SwiftUI_NetflixStyle

---
+++ SwiftUI/NetflixStyle/NetflixStyle/ContentCollectionViewHeader.swift
@@ -0,0 +1,28 @@
+//
+//  ContentCollectionViewHeader.swift
+//  NetflixStyle
+//
+//  Created by SamoYed on 2023/04/16.
+//
+
+import UIKit
+
+class ContentCollcetionViewHeader: UICollectionReusableView {
+    let sectionNameLabel = UILabel()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        
+        sectionNameLabel.font = .systemFont(ofSize: 17, weight: .bold)
+        sectionNameLabel.textColor = .white
+        sectionNameLabel.sizeToFit()
+        
+        addSubview(sectionNameLabel)
+        
+        sectionNameLabel.snp.makeConstraints{
+            $0.centerY.equalToSuperview()
+            $0.top.bottom.leading.equalToSuperview().offset(10)
+        }
+    }
+}
